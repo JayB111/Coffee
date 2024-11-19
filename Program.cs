0a5a0f1894122984ddf529a03d57be8057178777
JayB111 <jay.ljungby@gmail.com>
2024-11-19 15:50:25 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class CafeBusiness
+{
+    public static decimal CalculateVAT(string productType, int price)
+    {
+        int vatRate;
+
+        if (productType == "music")
+        {
+            vatRate = 6; 
+        }
+        else if (productType == "food")
+        {
+            vatRate = 12;
+        }
+        else if (productType == "alcohol")
+        {
+            vatRate = 25;
+        }
+        else
+        {
+            
+            throw new ArgumentException("Invalid product type. Choose 'music', 'food', or 'alcohol'.");
+        }
+
+        
+        decimal vatAmount = price + vatRate / 100m * price;
+        
+
+        return vatAmount;
+    }
+
+    
+    static void Main()
+    {
+        decimal musicVAT = CalculateVAT("music", 100);
+        Console.WriteLine("VAT for music: " + musicVAT);  
+
+        decimal foodVAT = CalculateVAT("food", 200);
+        Console.WriteLine("VAT for food: " + foodVAT);   
+
+        decimal alcoholVAT = CalculateVAT("alcohol", 50);
+        Console.WriteLine("VAT for alcohol: " + alcoholVAT);  
+    }
+}
+
---
+++ ServiceMethods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2;
+
+//Assignment 1
+
+//food 12%
+//music 6%
+//alcohol 25%
+
+
